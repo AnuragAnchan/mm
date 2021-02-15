6769aca120979b65d77c17b29d1129ffecd03633
AnuragAnchan <73586365+AnuragAnchan@users.noreply.github.com>
2021-02-15 18:24:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,80 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1;
+var world,boy;
+
+function preload(){
+	boy=loadImage("images/boy.png");	
+	
+	
+  }
+
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
+
+	mango1=new mango(1100,100,30);
+	mango2=new mango(1200,120,30);
+	mango3=new mango(1000,110,30);
+	mango4=new mango(950,250,30);
+    mango5=new mango(1200,270,30);
+	mango6=new mango(1100,200,30);
+
+	treeObj=new tree(1050,580);
+
+	
+	groundObject=new ground(width/2,600,width,20);
+	
+	Engine.run(engine);
+
+}
+
+function draw() {
+
+  background(230);
+  //Add code for displaying text here!
+  image(boy ,200,340,200,300);
+  
+
+  
+
+  treeObj.display();
+  mango1.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
+  mango5.display();
+  mango6.display();
+
+  
+
+  groundObject.display();
+
+ 
+ 
+
+}
+
+function keyPressed(){
+	if(key === 32){
+	Matter.body.setPosition(stoneObj.body,{x:235,y:420})
+	launcherObject.attach(stoneObj.body);
+	}
+}
+function dectectollision(lstone,lmango){
+	mangoBodyPosition=lmango.body.position
+	stoneBodyPosition=lstone.body.position
+
+	var distance= dist(stoneBodyPosition.x,stoneBodyPosition.y,mangoBodyPosition.x,mangoBodyPosition.y)
+	if(distance<=lmango.r+lstone.r){
+		Matter.Body.setStatic(lmango.body,false);
+	}
+}
+
